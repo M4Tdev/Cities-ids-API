b2b8127e8ce8e41a4c1bd96b1a5738e7f1edecc0
M4T <mateo.lesiuk@gmail.com>
2019-02-28 17:13:22 +0100

    Change loading cities list file on server start instead of loading on every search

--- server.js
+++ server.js
@@ -5,27 +5,31 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.send(req.query);
-// });
+let citiesList;
+
+fs.readFile(path.join(__dirname, 'city.list.json'), (err, data) => {
+  if (err) throw err;
+  citiesList = data;
+});
 
 app.get('/', (req, res) => {
   if (req.query !== {} && req.query.q) {
-    fs.readFile(path.join(__dirname, 'city.list.json'), (err, data) => {
-      if (err) throw err;
-      const jsonData = JSON.parse(data);
-      const results = jsonData.filter(obj => {
-        // const regex = new RegExp(req.query.q, 'gi'); // will find users input if city or country name no matter where it is located in the name
-        const regex = new RegExp(`^${req.query.q}`, 'gi'); // will find users input if city or country name starts with it
-        return obj.name.match(regex) || obj.country.match(regex);
-      });
-      // filtering to not return duplicated values
-      const filtered = results.filter(
-        (location, i, arr) =>
-          i === arr.findIndex(item => location.name === item.name && location.country === item.country)
-      );
-      res.json(filtered);
+    const jsonData = JSON.parse(citiesList);
+    const results = jsonData.filter(obj => {
+      // const regex = new RegExp(req.query.q, 'gi'); // will find users input if city or country name no matter where it is located in the name
+      const regex = new RegExp(`^${req.query.q}`, 'gi'); // will find users input if city or country name starts with it
+      return obj.name.match(regex) || obj.country.match(regex);
     });
+    // filtering to not return duplicated values
+    const filtered = results.filter(
+      (location, i, arr) =>
+        i ===
+        arr.findIndex(
+          item =>
+            location.name === item.name && location.country === item.country
+        )
+    );
+    res.json(filtered);
   } else {
     res.status(400).json({ msg: 'Url was not found on the server' });
   }
